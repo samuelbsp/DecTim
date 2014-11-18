16068a9d27afbcc266d6cb277326c507d99d90f7
Cegep Edouard-Montpetit <anne.roy@cegepmontpetit.ca>
2014-11-18 14:48:20 -0500

    Manquais 3 ficheirs à ma dernière version.

--- js/script.js
+++ js/script.js
@@ -8,8 +8,51 @@ $(document).ready(function () {
 	if (last_item == index){
 	resizeCssIndex();
 	}
-});
+
 function resizeCssIndex(){
 	$(".coverProjet").css("height", $(".imgProjet").height());
 	$(".projet").css("height", $(".imgProjet").height());
 }
+	var i=1;
+	$("#navProgramme"+i).css("border-bottom", "5px solid white");
+	function bougerNAV(){
+		for (var y=1;y<=5;y++){
+		$("#navProgramme"+y).css("border-bottom", "0px solid white");
+		$("#section"+y).removeClass("toggleProgramme");
+		}
+		$("#navProgramme"+i).css("border-bottom", "5px solid white");
+		$("#section"+i).toggleClass("toggleProgramme");
+	}
+
+$("#navProgramme1").click(function(){
+		i=1;
+		bougerNAV();
+});
+$("#navProgramme2").click(function(){
+		i=2;
+		bougerNAV();
+});
+$("#navProgramme3").click(function(){
+		i=3;
+		bougerNAV();
+});
+$("#navProgramme4").click(function(){
+		i=4;
+		bougerNAV();
+});
+$("#navProgramme5").click(function(){
+		i=5;
+		bougerNAV();
+});
+window.onresize = function(){
+	if (last_item == index){
+	resizeCssIndex();
+	}
+}
+window.onload = function(){
+	if (last_item == index){
+	resizeCssIndex();
+	}
+}
+
+});
